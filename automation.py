1d9e8ca0bbb60a52169112f9225a9d9d435970fc
arozrai <72703446+arozrai@users.noreply.github.com>
2021-08-21 11:44:00 +0100

    Add files via upload

--- automation.py
+++ automation.py
@@ -1,6 +1,6 @@
 import time
 
-lastHour = time.time()/60/60
+lastHour = time.time()/(60*24)
 
 def clock():
     currentTime = time.time()/60
@@ -8,8 +8,10 @@ def clock():
 
 def main():
     while(True):
-        hour = int((time.time()-lastHour)/60/60)
-        min = int((time.time()-lastHour)/60)
+        hour = int((time.time()/(60*24))-lastHour)
+        print(hour)
+        min = int((time.time()/60)-(lastHour*60))
+        print(min)
         if(min == min+1):
             clock()
         if(hour == hour+1):
